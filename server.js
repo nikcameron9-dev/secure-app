e9ff230dcf89bc0eec1d8688b738f894d959c6d9
Aneeqa Kamran <nikcameron9@gmail.com>
2026-04-27 04:46:34 +0500

    Initial commit - secure Node.js app with validation, JWT, helmet, and ZAP testing

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const helmet = require('helmet');
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'"],
+        imgSrc: ["'self'", "data:"],
+      },
+    },
+  })
+);
+
+// Home route
+app.get('/', (req, res) => {
+  res.send('Server is working');
+});
+
+// Register route
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!validator.isEmail(email)) {
+    return res.status(400).send('Invalid email');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const token = jwt.sign(
+    { email: email },
+    'secret123',
+    { expiresIn: '1h' }
+  );
+
+  res.send({
+    message: 'User registered',
+    token: token
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
